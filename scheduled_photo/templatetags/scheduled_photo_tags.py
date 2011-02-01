fa3659c3a7d8da189742bc5aac082f3b7952b87c
Thomas Hamlin <thomas@metaphorlab.com>
2011-02-01 13:58:39 -0800

    Fix 'scheduled_photo' name collision in template
    Also add csrf_token to 'add' form.

--- scheduled_photo/templatetags/scheduled_photo_tags.py
+++ scheduled_photo/templatetags/scheduled_photo_tags.py
@@ -14,7 +14,7 @@ def scheduled_photo(thumb_size=None, use_cache=True, cache_seconds=3600):
         except:
             thumb_size = "120x120"
     photo = ScheduledPhoto.for_today(use_cache=use_cache, cache_seconds=cache_seconds)
-    return {'scheduled_photo': photo,
+    return {'photo': photo,
             'thumb_size': thumb_size,}
 
 
