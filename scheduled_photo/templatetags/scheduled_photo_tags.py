351b4399c513b19c41f4f7d2efb1e4d9407fa96c
Thomas Hamlin <thomas@metaphorlab.com>
2011-03-18 13:37:05 -0700

    Make urls generic and simplify templatetag a bit

--- scheduled_photo/templatetags/scheduled_photo_tags.py
+++ scheduled_photo/templatetags/scheduled_photo_tags.py
@@ -9,10 +9,7 @@ register = Library()
 @register.inclusion_tag('scheduled_photo/view.html')
 def scheduled_photo(thumb_size=None, use_cache=True, cache_seconds=3600):
     if not thumb_size:
-        try:
-            thumb_size = settings.SCHEDULED_PHOTO_SIZE
-        except:
-            thumb_size = "120x120"
+        thumb_size = getattr(settings, 'SCHEDULED_PHOTO_SIZE', "120x120")
     photo = ScheduledPhoto.for_today(use_cache=use_cache, cache_seconds=cache_seconds)
     return {'photo': photo,
             'thumb_size': thumb_size,}
--- scheduled_photo/urls.py
+++ scheduled_photo/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls.defaults import patterns, url
-import views
+from django.conf.urls.defaults import *
 
-urlpatterns = patterns('',
-    url(r'^photo-of-day/add$', views.add, name='scheduled_photo_add'),
-    url(r'^photo-of-day/delete/(?P<photo_id>\d+)$', views.delete, name='scheduled_photo_delete'),
-    url(r'^photo-of-day/edit/(?P<photo_id>\d+)$', views.edit, name='scheduled_photo_edit'),
+
+urlpatterns = patterns('scheduled_photo.views',
+    url(r'^add$', 'add', name='scheduled_photo_add'),
+    url(r'^edit/(?P<photo_id>\d+)$', 'edit', name='scheduled_photo_edit'),
+    url(r'^delete/(?P<photo_id>\d+)$', 'delete', name='scheduled_photo_delete'),
 )
